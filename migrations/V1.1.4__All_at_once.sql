d7d884a70f02ff5acea51b20a98b9eadefe24d84
DEEPAK894 <74669196+DEEPAK894@users.noreply.github.com>
2024-01-19 14:24:32 +0530

    Update V1.1.4__All_at_once.sql

--- migrations/V1.1.4__All_at_once.sql
+++ migrations/V1.1.4__All_at_once.sql
@@ -1,2 +1,12 @@
-DELETE FROM DEMO.HELLO_WORLD
-WHERE LAST_NAME = 'sharma';
+CREATE SCHEMA DEMO_ONE;
+CREATE TABLE All_In_One
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO All_In_One
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;
